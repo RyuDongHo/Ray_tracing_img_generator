5969a731f8d9406a230d730fc05250810ef0da72
unknown <thffhahs03@gmail.com>
2026-01-09 00:57:15 +0900

    feat: hit_sphere ray광선과 구체가 만나는 경우를 계산하여 구체를 그리는 함수 추가

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,29 @@
 #include "vec3.h"
 #include "ray.h"
 
+/*
+구체의 중점 center, 
+카메라의 위치에서 view port로 투영되는 직선을 P(t) = Q + dt 라고 하자 (Q는 카메라 위치, d는 단위 방향 벡터, t는 실수)
+이때 직선 P(t)가 구체와 만나는 점 P에 대해서 |P - center| = radius 를 만족한다.
+정리하면 (P - C) * (P - C) = r^2 을 만족하고
+이를 전개하면 다음과 같은 2차 방정식을 얻는다.
+t^2 * (d * d) - 2t * d * (C - Q) + (C - Q)*(C - Q) - r^2 = 0
+이때 근의공식에 의해 판별식 D = b^2 - 4ac 가 0 이상이면 직선과 구체가 만난다.
+이를 이용하여 ray가 구체와 만나는지 확인하는 hit_sphere 함수를 아래와 같이 작성할 수 있다.
+*/
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);

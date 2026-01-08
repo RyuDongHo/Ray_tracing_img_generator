332033ad5e8469680282e294afc229fdbe036a1e
unknown <thffhahs03@gmail.com>
2026-01-08 22:09:21 +0900

    feat: vec3 vec3 class 추가

---
+++ include/vec3.h
@@ -0,0 +1,46 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+
+class vec3 {
+  public:
+    double e[3];
+
+    vec3();
+    vec3(double e0, double e1, double e2);
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const;
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    vec3& operator+=(const vec3& v);
+    vec3& operator*=(double t);
+    vec3& operator/=(double t);
+
+    double length() const;
+    double length_squared() const;
+};
+
+// point3 is just an alias for vec3, but useful for geometric clarity in the code.
+using point3 = vec3;
+
+
+// Vector Utility Functions
+
+std::ostream& operator<<(std::ostream& out, const vec3& v);
+vec3 operator+(const vec3& u, const vec3& v);
+vec3 operator-(const vec3& u, const vec3& v);
+vec3 operator*(const vec3& u, const vec3& v);
+vec3 operator*(double t, const vec3& v);
+vec3 operator*(const vec3& v, double t);
+vec3 operator/(const vec3& v, double t);
+double dot(const vec3& u, const vec3& v);
+vec3 cross(const vec3& u, const vec3& v);
+vec3 unit_vector(const vec3& v);
+
+#endif
---
+++ src/vec3.cpp
@@ -0,0 +1,81 @@
+#include "vec3.h"
+#include <cmath>
+
+// Constructors
+vec3::vec3() : e{0, 0, 0} {}
+
+vec3::vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+// Member functions
+vec3 vec3::operator-() const {
+    return vec3(-e[0], -e[1], -e[2]);
+}
+
+vec3& vec3::operator+=(const vec3& v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
+
+vec3& vec3::operator*=(double t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+vec3& vec3::operator/=(double t) {
+    return *this *= 1 / t;
+}
+
+double vec3::length() const {
+    return std::sqrt(length_squared());
+}
+
+double vec3::length_squared() const {
+    return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+}
+
+// Utility functions
+std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+vec3 operator*(double t, const vec3& v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+vec3 operator/(const vec3& v, double t) {
+    return (1 / t) * v;
+}
+
+double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+vec3 unit_vector(const vec3& v) {
+    return v / v.length();
+}
